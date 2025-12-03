25cee7f14057749fcb6a5b6cf22ee457319f4e3b
PantelisTs <pantsotsgr@gmail.com>
2025-12-03 21:08:11 +0200

    fix: add new product on main.

--- src/gr/aueb/cf/cf9/ch11/Main.java
+++ src/gr/aueb/cf/cf9/ch11/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         Product milk = new Product(1L, "Milk A", "Light Milk", 1.5, 2, true);
+        Product juice = new Product(2L, "Juice B", "Orange Juice", 2.5, 1, true);
         System.out.println(milk.getDescription());
+
 
         User user = new User();
         User andreas22 = new User(1L, "Andreas", "Papadopoulos", "andrew", "12345", true);
