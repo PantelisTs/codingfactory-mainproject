7529b8e4a11bb73ba746caf14e984a27e0f8387b
PantelisTs <pantsotsgr@gmail.com>
2025-12-03 20:12:38 +0200

    feat: ch11, new class student and a main to understand it more

---
+++ src/gr/aueb/cf/cf9/ch11/Student.java
@@ -0,0 +1,67 @@
+package gr.aueb.cf.cf9.ch11;
+
+/**
+ * Java Bean.
+ * Data class που έχει
+ *  - private πεδία.
+ *  - Default constructor.
+ *  - getters & setters με συμβάσεις.
+ */
+public class Student {
+    private static int studentsCount = 0;
+
+    // Static block (αντί για το από πάνω)
+    static {
+        studentsCount = 0;
+    }
+
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    //Default constructor
+    public Student () {
+        studentsCount++;
+
+    }
+
+    //Overloaded constructor
+    public Student (int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        studentsCount++;
+    }
+
+
+// Public Getters και Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+
+    public static int getStudentsCount() {
+        return studentsCount;
+    }
+}
