3f3330a56565e9a4747d51795a35bc4f9ef202f6
PantelisTs <pantsotsgr@gmail.com>
2025-12-08 21:05:15 +0200

    feat: add main for usage of the Account Public API, creates aliceAccount, tries some deposits and withdraws and catches exception of wrong SSN.

---
+++ src/gr/aueb/cf/cf9/ch12/Main.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.cf9.ch12;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        Account aliceAccount = new Account(1L, "GR12345", "Alice", "W.", "A12345", 100);
+
+        try {
+            aliceAccount.deposit(500);
+            System.out.println(aliceAccount.accountToString());
+
+            aliceAccount.withdraw(100, "A12345");
+            System.out.println(aliceAccount.accountToString());
+
+            aliceAccount.withdraw(100, "A123456789");
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
