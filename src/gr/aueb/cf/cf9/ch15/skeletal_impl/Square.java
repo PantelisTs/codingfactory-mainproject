bbd8fd501b430b6dadd8c94a2460cd2ea043fa43
PantelisTs <pantsotsgr@gmail.com>
2025-12-10 22:53:46 +0200

    fix: add usage inside Square.

--- src/gr/aueb/cf/cf9/ch15/skeletal_impl/Square.java
+++ src/gr/aueb/cf/cf9/ch15/skeletal_impl/Square.java
@@ -1,4 +1,10 @@
 package gr.aueb.cf.cf9.ch15.skeletal_impl;
 
-public class Square extends AbstractRectangle{
+public class Square extends AbstractRectangle {
+
+    // Now accepts both width and height independently
+    public Square(double width, double height) {
+        setWidth(width);
+        setHeight(height);
+    }
 }
